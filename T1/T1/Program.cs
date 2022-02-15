730906989d6e997e69fb58e90c35fc1d73f9c931
Jyri Lindroos <jyri.lindroos@keuda.fi>
2022-02-15 11:32:33 +0200

    ekat C# harjoitukset 15.2.2022

---
+++ T1/T1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String luku1, luku2;
+            int summa;
+            Console.Write("Anna 1. luku: ");
+            luku1 = Console.ReadLine();
+            Console.Write("Anna 2. luku: ");
+            luku2 = Console.ReadLine();
+            summa = Int32.Parse(luku1) + Int32.Parse(luku2);
+            Console.WriteLine(summa);
+            Console.ReadLine();
+        }
+    }
+}
